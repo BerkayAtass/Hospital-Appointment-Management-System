fe18d801382d20ad2c0363ad9daff6b84d181431
Berkay ATAŞ <74881380+BerkayAtass@users.noreply.github.com>
2023-06-04 06:46:52 +0300

    Add files via upload

---
+++ resources/views/layouts/frontbase.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+    <title>@yield("title")</title>
+
+    <link rel="stylesheet" href="../assets/css/maicons.css">
+
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+    <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+    <link rel="stylesheet" href="../assets/css/theme.css">
+    @yield("head")
+</head>
+
+<body>
+@include("home.header")
+
+@section('sidebar')
+    @include("home.sidebar")
+@show
+
+
+@yield('content')
+
+@include("home.footer")
+@yield('foot')
+</body>
+</html>
